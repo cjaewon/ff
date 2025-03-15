6da2232d377edb0c90b4700524a2386b543dbf99
cjaewon <jwn4492@outlook.com>
2025-03-15 16:31:01 +0900

    Fix code highlighting error

---
+++ cmd/code-css-gen.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/alecthomas/chroma/v2/formatters/html"
+	"github.com/alecthomas/chroma/v2/styles"
+)
+
+func main() {
+	style := styles.Get("github")
+	if style == nil {
+		log.Fatal("스타일을 찾을 수 없습니다.")
+	}
+
+	formatter := html.New(html.WithClasses(true))
+
+	f, err := os.Create("server/web/assets/code-styles.css")
+	if err != nil {
+		log.Fatalf("파일 생성 오류: %v", err)
+	}
+	defer f.Close()
+
+	err = formatter.WriteCSS(f, style)
+	if err != nil {
+		log.Fatalf("CSS 작성 오류: %v", err)
+	}
+
+	log.Println("CSS 파일이 성공적으로 생성되었습니다.")
+}
